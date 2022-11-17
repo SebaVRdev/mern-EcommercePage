68b749c8d0cc1e32836181c1c15f70e42cd34d52
Sebaarico <sebarm177@gmail.com>
2022-11-16 22:36:50 -1100

    Inicio Proyecto 16/11

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Inicio</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
